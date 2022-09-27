8b5d98b6dccc33b9792b4a9202e8651fb4579552
Mark Gardner <mark.gardner@sydney.edu.au>
2022-09-27 16:42:50 +1000

    New Version of the Code
    Not just one script but multiple scripts

--- DeformHeadCT/deformation.py
+++ DeformHeadCT/deformation.py
@@ -4,13 +4,197 @@ Created on Wed Jul 20 15:26:22 2022
 
 @author: mgar5380
 """
+#External Modules
 import os
+import re
 import SimpleITK as sitk
 import numpy as np
-#from platipy.imaging.registration.registration import apply_field
-from platipy.imaging.registration.utils import apply_transform
+from pathlib import Path
 from scipy.spatial.transform import Rotation
+
+#Platipy Modules
+#from platipy.imaging.registration.registration import apply_field
+from platipy.imaging.generation.dvf import generate_field_shift
+from platipy.imaging.registration.utils import apply_transform
 from platipy.imaging.generation.mask import get_bone_mask
+from platipy.imaging.generation.mask import get_external_mask
+
+#Internal Modules
+from DeformHeadCT.utils import get_head_mask
+
+"""
+TODO
+Add option for gaussian smoothing
+"""
+
+class HeadDeformation():
+    
+    def __init__(self,nifti_directory,patientunderscore,structShiftFlag = 0):
+        """
+        Initialise class
+
+        Parameters
+        ----------
+        nifti_directory : Path
+            Path to temporary directory.
+        patientunderscore : str
+            patient identifier.
+        structShiftFlag : bool, optional
+            If 1 (Or True or whatever) the structures will be shifted. The default is 0.
+
+        Returns
+        -------
+        None.
+
+        """
+        #Define nifti volumes to work with     
+        ct_path = next(iter(nifti_directory.glob(f"{patientunderscore}/IMAGES/*CT*.nii.gz")))
+        
+        # Read in image    
+        self.image_ct = sitk.ReadImage(str(ct_path))
+        
+        if structShiftFlag:
+            self.structure_paths = nifti_directory.glob(f"{patientunderscore}/STRUCTURES/*.nii.gz")
+        else:
+            self.structure_paths = ''
+    
+        #Define dvf_field
+        self.dvf_field = sitk.Image(self.image_ct.GetSize(), sitk.sitkVectorFloat64);
+        sitk.Image.CopyInformation(self.dvf_field,self.image_ct)    
+    
+    def ApplyHeadRotation(self,coordinates_cutoff,angles,axes,point_of_rotation):
+        """
+        Generates a deformation to rotate the head, which is stored in dvf_field
+
+        Parameters
+        ----------
+        coordinates_cutoff: array
+            2x3 array annotating the co-ordinates that seperate the head and the shoulders. See the wiki for more information.
+        angles : (float | arr)
+            angles of rotation. If empty no head rotation will be done. 
+        axes : array
+            nx3 array where the nth element is the axis around which the volume will rotate. See wiki for more info.
+        point_of_rotation : array
+            nx3 array where each nth element is the centre voxel around which the head rotations will occur. .
+
+        Returns
+        -------
+        Return 0 if no error, returns -1 if there is a problem with the input variables
+
+        """
+        if not angles:
+            print('"angles" variable cannot be empty')
+            return -1
+        
+        #Get Masks
+        # Automatically generate external mask
+        struct_external = get_external_mask(self.image_ct)
+        
+        #generate head mask using list of coordinates as cutoff points
+        head_mask = get_head_mask(struct_external,coordinates_cutoff, self.image_ct)            
+        
+        #Apply the rotations 
+        try: 
+            len(axes[0])
+            LoopNum = len(axes)
+        except:
+            LoopNum = 1
+            
+        try:
+            angles[0]
+        except:
+            angles = [angles]
+        
+        #Define variables. Do they get used? Who knows. But they are living their best life.   
+        Mask_deformed = [];
+        dvf_transform = [];
+        
+        for i in range(0,LoopNum):
+            if LoopNum > 1:
+                axisLoop = axes[i]
+                RotationPt = point_of_rotation[i]
+            else:
+                axisLoop = axes
+                RotationPt = point_of_rotation
+                
+            Mask_deformed_loop, dvf_transform_loop, dvf_field_loop, RotationTForm = generate_field_rotation(
+                self.image_ct,
+                head_mask,
+                tuple(RotationPt),
+                axis_of_rotation=axisLoop,
+                angle=angles[i]*np.pi/180,
+                gaussian_smooth=0
+                #mask_bend_from_reference_point = coordinates_cutoff
+            )
+            Mask_deformed.append(Mask_deformed_loop)
+            dvf_transform.append(dvf_transform_loop)
+            self.dvf_field = self.dvf_field + dvf_field_loop   
+    
+        return 0
+    
+    def ApplyGTVShift(self,Structure_Shift,Structure_Names):
+        """
+        Rigidly Shift the structures defined by Structure_Names by the amount described in Structure_Shift
+
+        Parameters
+        ----------
+        Structure_Shift : array
+            1x3 array describing the magnitude of the rigid displacement of the structures in "Structure_Names".
+        Structure_Names : (str | array),
+            The structures to be shifted.
+
+        Returns
+        -------
+        None.
+
+        """
+        structures = {re.findall(".*_RTSTRUCT_(.*).nii.gz", p.name)[0]: sitk.ReadImage(str(p)) for p in self.structure_paths}
+        
+        if np.size(Structure_Shift[0]) > 1:
+            for i in range(0,len(Structure_Shift)):
+                
+                if Structure_Names[i] not in structures:
+                    print('No Structure {}'.format(Structure_Names[i]))
+                else:
+                
+                    Mask_shift, dvf_shift, dvf_field_shift = generate_field_shift(
+                        mask_image = structures[Structure_Names[i]],
+                        vector_shift = tuple(Structure_Shift[i]),
+                        gaussian_smooth = 2
+                    )
+                    #dvf_transform.append(dvf_shift)
+                    
+                    dvfLoop = sitk.Cast(dvf_field_shift, sitk.sitkVectorFloat32)  
+                    dvfLoop.CopyInformation(self.dvf_field)                           
+                    
+                    try:
+                        self.dvf_field = self.dvf_field + dvf_field_shift
+                    except:
+                        self.dvf_field = self.dvf_field + sitk.Cast(dvfLoop,sitk.sitkVectorFloat64) 
+                    #dvf_transform.append(dvf_shift)
+                    #Mask_deformed.append(Mask_shift)
+        else:
+            if Structure_Names not in structures:
+                print('No Structure {}'.format(Structure_Names))
+            else:
+                Mask_shift, dvf_shift, dvf_field_shift = generate_field_shift(
+                    mask_image = structures[Structure_Names],
+                    vector_shift = tuple(Structure_Shift),
+                    gaussian_smooth = 2
+                )                
+                #dvf_transform.append(dvf_shift)
+                #Mask_deformed.append(Mask_shift)
+                
+                dvfLoop = sitk.Cast(dvf_field_shift, sitk.sitkVectorFloat32)  
+                dvfLoop.CopyInformation(self.dvf_field)     
+            
+                try:
+                    self.dvf_field = self.dvf_field + dvf_field_shift
+                except:
+                    self.dvf_field = self.dvf_field + sitk.Cast(dvfLoop,sitk.sitkVectorFloat64) 
+
+
+
 
 def generate_field_rotation(
     reference_image,
---
+++ tests/test_CTHeadDeformation.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 19 10:30:33 2022
+
+@author: mgar5380
+"""
+
+from pathlib import Path
+import sys
+import numpy as np
+from shutil import rmtree
+import glob
+import json
+
+'''
+For testing/ example purposes it's safest to manually append the path variable to ensure our 
+package will always be found. This isn't necessary once we actually install the package 
+because installation in python essentially means "copying the package to a place where it can be found"
+'''
+this_file_loc = Path(__file__)
+sys.path.insert(0, str(this_file_loc.parent.parent))
+# note: insert(0) means that the path is above is the FIRST place that python will look for imports
+# sys.path.append means that the path above is the LAST place
+from DeformHeadCT.VolumeInfo import VolumeDeformation
+from DeformHeadCT.DataPreparation import (
+    MoveDCMFiles,
+    GetPointOfRotation
+    )
+
+def test_VolumeInfoInit():
+    '''
+    Test whether the initialisation of the VolumeInfo class creates the class variables properly
+    '''
+    
+    patient_id = 'zz_Elshin'
+    patientunderscore = 'zz_Elshin'
+    
+    axes = [-1,0,0]
+    angles = [2.5]
+    Structure_Shift = [1,0,0]
+    coordinates_cutoff = [[85, 303, 261], [72, 202, 254]]
+    
+    VertDict = {}
+    VertDict['Oc-C1'] = [118, 239, 258]
+    VertDict['C1-C2'] = [111, 235, 257]
+    VertDict['C2-C3'] = [101, 230, 256]
+    
+    InputDir = 'examples/PhantomDicomData'
+    StructDir = 'test'
+    OutputDir = 'examples/TestingGitHubThing'
+    nifti_directory = 'examples/TestingTempDir'
+    Structure_Names = 'test'
+    
+    #Create variable using custom inputs
+    TestInfo = VolumeDeformation(patient_id = patient_id,patientunderscore = patientunderscore, axes = axes, angles = angles,
+                                 Structure_Shift = Structure_Shift,OutputDir = OutputDir, nifti_directory = nifti_directory,
+                                 Structure_Names = Structure_Names,InputDir = InputDir,StructDir = StructDir,
+                                 coordinates_cutoff=coordinates_cutoff,VertDict=VertDict)
+    
+    assert TestInfo.patient_id == patient_id, 'patient_id variables are not equal {} {}'.format(TestInfo.patient_id,patient_id)
+    assert TestInfo.patientunderscore == patientunderscore, 'patientunderscore variables are not equal {} {}'.format(TestInfo.patientunderscore,patientunderscore)
+    
+    assert TestInfo.axes == axes, 'axes variables are not equal {} {}'.format(TestInfo.axes,axes)
+    assert TestInfo.angles == angles, 'angles variables are not equal {} {}'.format(TestInfo.angles,angles)
+    assert TestInfo.Structure_Shift == Structure_Shift, 'Structure_Shift variables are not equal {} {}'.format(TestInfo.Structure_Shift,Structure_Shift)
+    assert TestInfo.coordinates_cutoff == coordinates_cutoff, 'co-ordinates_cutoff variables are not equal {} {}'.format(TestInfo.coordinates_cutoff,coordinates_cutoff)
+    assert TestInfo.point_of_rotation == [VertDict['C1-C2']], 'Point(s) of Rotation are not equal {} {}'.format(TestInfo.point_of_rotation,VertDict['Oc-C1'])
+    
+    assert TestInfo.InputDir == InputDir, 'InputDir variables are not equal {} {}'.format(TestInfo.InputDir,InputDir)
+    assert TestInfo.StructDir == StructDir, 'StructDir variables not are equal {} {}'.format(TestInfo.StructDir,StructDir)
+    assert TestInfo.OutputDir == OutputDir, 'OutputDir variables not are equal {} {}'.format(TestInfo.OutuputDir,OutputDir)
+    assert TestInfo.nifti_directory == Path(nifti_directory), 'nifti_directory variables are not equal {} {}'.format(TestInfo.nifti_directory,nifti_directory)
+    assert TestInfo.Structure_Names == Structure_Names, 'Structure_Names variables are not equal {} {}'.format(TestInfo.Structure_Names,Structure_Names)
+    
+    JsonInfoFile = 'examples/OneAxisRotation.json'
+    
+    #Create variable using a json file
+    JsonTestInfo = VolumeDeformation(InfoFile=JsonInfoFile)
+    
+    assert JsonTestInfo.patient_id == patient_id, 'patient_id variables are not equal {} {}'.format(JsonTestInfo.patient_id,patient_id)
+    assert JsonTestInfo.patientunderscore == patientunderscore, 'patientunderscore variables are not equal {} {}'.format(JsonTestInfo.patientunderscore,patientunderscore)
+    
+    assert JsonTestInfo.axes == axes, 'axes variables are not equal {} {}'.format(JsonTestInfo.axes,axes)
+    assert JsonTestInfo.angles == angles, 'angles variables are not equal {} {}'.format(JsonTestInfo.angles,angles)
+    #assert JsonTestInfo.Structure_Shift == Structure_Shift, 'Structure_Shift variables are not equal {} {}'.format(JsonTestInfo.Structure_Shift,Structure_Shift)
+    assert JsonTestInfo.coordinates_cutoff == coordinates_cutoff, 'co-ordinates_cutoff variables are not equal {} {}'.format(JsonTestInfo.coordinates_cutoff,coordinates_cutoff)
+    assert JsonTestInfo.point_of_rotation == [VertDict['C1-C2']], 'Point(s) of Rotation are not equal {} {}'.format(JsonTestInfo.point_of_rotation,VertDict['Oc-C1'])
+    
+    
+    assert JsonTestInfo.InputDir == InputDir, 'InputDir variables are not equal {} {}'.format(JsonTestInfo.InputDir,InputDir)
+    #assert JsonTestInfo.StructDir == StructDir, 'StructDir variables not are equal {} {}'.format(JsonTestInfo.StructDir,StructDir)
+    assert JsonTestInfo.OutputDir == OutputDir, 'OutputDir variables not are equal {} {}'.format(JsonTestInfo.OutuputDir,OutputDir)
+    assert JsonTestInfo.nifti_directory == Path(nifti_directory), 'nifti_directory variables are not equal {} {}'.format(JsonTestInfo.nifti_directory,nifti_directory)
+    #assert JsonTestInfo.Structure_Names == Structure_Names, 'patientunderscore variables are not equal {} {}'.format(JsonTestInfo.Structure_Names,Structure_Names)
+
+    assert Path(nifti_directory).is_dir(),'TempDirectory {} not created'.format(JsonTestInfo.nifti_directory)
+    assert Path(OutputDir).is_dir(),'InputDirectory {} not created'.format(JsonTestInfo.OutputDir)
+    
+    rmtree(nifti_directory)
+    rmtree(Path(OutputDir))
+    
+def test_RotationPoint():
+    
+    VertDict = {}
+    VertDict['Oc-C1'] = [118, 239, 258]
+    VertDict['C1-C2'] = [111, 235, 257]
+    VertDict['C2-C3'] = [101, 230, 256]
+
+    VertDict['axes'] = [[1,0,0],[0,1,0],[0,0,1]]
+    VertDict['angles'] = [1,1,1]    
+    
+    point_of_rotation = GetPointOfRotation(VertDict)
+    
+    assert point_of_rotation[0] == VertDict['C1-C2'], 'Error in point_of_rotation[0]'
+    assert point_of_rotation[1] == VertDict['C2-C3'], 'Error in point_of_rotation[1]'
+    assert point_of_rotation[2] == VertDict['Oc-C1'], 'Error in point_of_rotation[2]'
+    
+
+    
+    
+    
