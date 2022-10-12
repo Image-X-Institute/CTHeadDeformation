23f95193df407575aa4f9bdab186eb98d5aac263
Mark Gardner <86278062+MarkGardnerUSyd@users.noreply.github.com>
2022-10-12 14:13:42 +1100

    Update test_CTHeadDeformation.py

--- tests/test_CTHeadDeformation.py
+++ tests/test_CTHeadDeformation.py
@@ -147,7 +147,7 @@ def test_PrepareDcmData():
         
         assert input_dcm_ct_dir + '/' + FileName, '{} file does not exist'.format(FileName)
 
-    print(string(JsonTestInfo.nifti_directory) + f"{JsonTestInfo.patientunderscore}/IMAGES")
+    print(str(JsonTestInfo.nifti_directory) + f"{JsonTestInfo.patientunderscore}/IMAGES")
     assert Path(str(JsonTestInfo.nifti_directory) + f"{JsonTestInfo.patientunderscore}/IMAGES").is_dir(),"CT Path Not created"
     ct_path = next(iter(JsonTestInfo.nifti_directory.glob(f"{JsonTestInfo.patientunderscore}/IMAGES/*CT*.nii.gz")))
     print(ct_path)
